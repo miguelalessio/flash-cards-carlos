b9a69e28aee9f49aabaab12ba01ab41762b9ff5e
miguelalessio <145574803+miguelalessio@users.noreply.github.com>
2024-11-08 11:06:02 -0300

    08/11

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
     'Esporte',
-    'Quantos gols PELÉ marcou em sua carreira'
+    'Quantos gols PELÉ marcou em sua carreira',
     '1283'
 )
 
 criaCartao(
-    'História'
-    'Quantos anos durou a guerra fria'
+    'História',
+    'Quantos anos durou a guerra fria',
     'Durou de 12 de março de 1947 ate 3 de dezembro de 1989'
 )
 
 criaCartao(
-    'História'
-    'Qual o nome de Dom pedro 2'
+    'História',
+    'Qual o nome de Dom pedro 2',
     'Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel Gabriel Rafael Gonzaga'
 )
 
 criaCartao(
-    'Conhecimentos gerais'
-    'Quantos litros de sangue uma pessoa adulta tem?'
+    'Conhecimentos gerais',
+    'Quantos litros de sangue uma pessoa adulta tem?',
     'Tem de 4 a 6 litros de sangue'
 )
 
