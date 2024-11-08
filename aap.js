28c0365e23d5c102c2ff25f085c8dadecfed8444
miguelalessio <145574803+miguelalessio@users.noreply.github.com>
2024-11-08 11:34:30 -0300

    08/11

--- aap.js
+++ aap.js
@@ -21,8 +21,9 @@ function criaCartao(categoria, pergunta, resposta) {
         respostaEstaVisivel = !respostaEstaVisivel;
         cartao.classList.toggle('active', respostaEstaVisivel);
     }
-
+    
     cartao.addEventListener('click', viraCartao);
+
 
     container.appendChild(cartao);
 }
